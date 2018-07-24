0f1daf13f85087318d345788e40289b825b624e6
dokjin.yoon <dokjin@gmail.com>
2018-07-24 21:24:23 +0900

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,9 +2,9 @@
 // 자신의 서버 설정에 맞춰 설정
 
 module.exports = {
-    host     : 'saesolsoft.co.kr',
-    user     : 'dokjin2',
-    password : '1234',
+    host     : '',
+    user     : '',
+    password : '',
     port     : 3306,
-    database : 'bnicorp'
+    database : ''
   };
